7fd18bcd2c14ebaf4837d8fc6c390ff9b13e963f
Marcos Kazimoto <marcos.kazimoto@totvs.com.br>
2023-07-10 23:31:58 -0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ConsoleApp2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Definir a sala e a posição inicial do robô
+      char[,] sala = new char[,]
+      {
+            { 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' },
+            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', ' ', ' ', 'X' },
+            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', ' ', ' ', 'X' },
+            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', ' ', ' ', 'X' },
+            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', ' ', ' ', 'X' },
+            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', ' ', ' ', 'X' },
+            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', ' ', ' ', 'X' },
+            { 'X', ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+            { 'X', ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+            { 'X', ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+            { 'X', ' ', ' ', ' ', 'X', 'X', 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+            { 'X', ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+            { 'X', ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', 'X', 'X', ' ', ' ', ' ', 'X' },
+            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', 'X', ' ', ' ', ' ', 'X' },
+            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', 'X', ' ', ' ', ' ', 'X' },
+            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', 'X', ' ', ' ', ' ', 'X' },
+            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', 'X', 'X', ' ', ' ', ' ', 'X' },
+            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+            { 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' }
+      };
+      int startX = 1;
+      int startY = 1;
+
+      // Executar o algoritmo de busca em profundidade
+      BuscaProfundidade(sala, startX, startY);
+
+      // Imprimir a sala após a busca
+      ImprimirSala(sala);
+    }
+
+    static void BuscaProfundidade(char[,] sala, int x, int y)
+    {
+      int altura = sala.GetLength(0);
+      int largura = sala.GetLength(1);
+
+      // Verificar se a posição atual é válida
+      if (x < 0 || x >= largura || y < 0 || y >= altura || sala[y, x] != ' ')
+      {
+        return;
+      }
+
+      // Marcar a posição atual como visitada
+      sala[y, x] = '*';
+      ImprimirSala(sala);
+
+      // Chamar recursivamente a busca em profundidade para as posições adjacentes
+      BuscaProfundidade(sala, x - 1, y); // Esquerda
+      BuscaProfundidade(sala, x + 1, y); // Direita
+      BuscaProfundidade(sala, x, y - 1); // Cima
+      BuscaProfundidade(sala, x, y + 1); // Baixo
+    }
+
+    static void ImprimirSala(char[,] sala)
+    {
+      Console.Clear();
+
+      int altura = sala.GetLength(0);
+      int largura = sala.GetLength(1);
+
+      for (int y = 0; y < altura; y++)
+      {
+        for (int x = 0; x < largura; x++)
+        {
+          Console.Write(sala[y, x]);
+        }
+        Console.WriteLine();
+      }
+      Thread.Sleep(100);
+    }
+  }
+}
