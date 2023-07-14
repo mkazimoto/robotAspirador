bf90282529521ce3ce7caf5d89650d4252f2124a
Marcos Kazimoto <marcos.kazimoto@totvs.com.br>
2023-07-14 17:26:17 -0300

    Add files via upload

---
+++ ConsoleApp2/Dijkstra.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+  /// <summary>
+  /// Algoritimo do menor caminho Dijkstra A*
+  /// </summary>
+  public class Dijkstra
+  {
+    private int maxY;
+    private int maxX;
+    private char[,] mapaSala;
+    
+    public Dijkstra(char[,] mapaSala)
+    {
+      maxY = mapaSala.GetLength(0);
+      maxX = mapaSala.GetLength(1);
+      this.mapaSala = mapaSala;
+    }
+
+    /// <summary>
+    /// Retorna o menor caminho entre 2 pontos
+    /// </summary>
+    /// <param name="posicaoInicial"></param>
+    /// <param name="posicaoFinal"></param>
+    /// <returns></returns>
+    public List<Point> GetPath(Point posicaoInicial, Point posicaoFinal)
+    {
+      // Pilha de pontos para memorizar as posições pendentes
+      Stack<Point> pilhaPosicao = new Stack<Point>();
+
+      int totalNodos = maxY * maxX;
+
+      int[] distance = new int[totalNodos];
+      int[] parent = new int[totalNodos];
+      bool[] visited = new bool[totalNodos];
+
+      for (int i = 0; i < totalNodos; i++)
+      {
+        distance[i] = int.MaxValue;
+        parent[i] = -1;
+        visited[i] = false;
+      }
+
+      int sourceIndex = GetIndex(posicaoInicial);
+      int destinationIndex = GetIndex(posicaoFinal);
+
+      distance[sourceIndex] = 0;
+
+      pilhaPosicao.Push(posicaoInicial);
+
+      while (!visited[destinationIndex] && pilhaPosicao.Count > 0)
+      {
+        var position = pilhaPosicao.Pop();
+        int positionIndex = GetIndex(position);
+
+        // Marca nodo como visitado
+        visited[positionIndex] = true;
+
+        List<Point> listProximaPosicao = GetProximaPosicao(position, posicaoFinal, visited);
+
+        foreach (var proximo in listProximaPosicao)
+        {
+          int neighborIndex = GetIndex(proximo);
+          int totalDistance = distance[positionIndex] + 1;
+
+          if (!visited[neighborIndex] && 
+              mapaSala[proximo.Y, proximo.X] != '#' && 
+              totalDistance < distance[neighborIndex])
+          {
+            pilhaPosicao.Push(proximo);
+
+            distance[neighborIndex] = totalDistance;
+            parent[neighborIndex] = positionIndex;
+          }
+        }
+      }
+
+      return ReconstructPath(parent, posicaoInicial, posicaoFinal);
+    }
+
+    /// <summary>
+    /// Retorna o índice do ponto
+    /// </summary>
+    /// <param name="position"></param>
+    /// <returns></returns>
+    private int GetIndex(Point position)
+    {
+      return position.Y * maxX + position.X;
+    }
+
+    /// <summary>
+    /// Retorna o ponto do índice
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    private Point GetCoordinates(int index)
+    {
+      int row = index / maxX;
+      int column = index % maxX;
+      return new Point(column, row);
+    }
+    
+    /// <summary>
+    /// Retorna as posições vizinhas
+    /// </summary>
+    /// <param name="row"></param>
+    /// <param name="column"></param>
+    /// <returns></returns>
+    private List<Point> GetProximaPosicao(Point position, Point destination, bool[] visited)
+    {
+      List<(Point position, double distance)> neighbors = new List<(Point position, double distance)>();
+
+      // Considerando movimentos nos sentidos: cima, baixo, esquerda e direita
+      int[] dX = { -1, 1, 0, 0 };
+      int[] dY = { 0, 0, -1, 1 };
+
+      for (int i = 0; i < 4; i++)
+      {
+        var newPosition = new Point(position.X + dX[i], position.Y + dY[i]);
+
+        if (IsValidPosition(newPosition, visited))
+        {
+          var distance = GetDistance(newPosition, destination);
+          neighbors.Add((position: newPosition, distance: distance));
+        }
+      }
+
+      // Prioriza a menor distancia até o ponto de destino
+      return neighbors.OrderByDescending(p => p.distance).Select(p => p.position).ToList();
+    }
+
+    /// <summary>
+    /// Calcula a distancia entre 2 pontos (Teorema de Pitágoras)
+    /// </summary>
+    /// <param name="position1"></param>
+    /// <param name="position2"></param>
+    /// <returns></returns>
+    private double GetDistance(Point position1, Point position2)
+    {
+      return Math.Sqrt(Math.Pow(Math.Abs(position2.X - position1.X), 2) + Math.Pow(Math.Abs(position2.Y - position1.Y), 2));
+    }
+
+    /// <summary>
+    /// Verifica se é uma posição válida no mapa
+    /// </summary>
+    /// <param name="position"></param>
+    /// <param name="visited"></param>
+    /// <returns></returns>
+    private bool IsValidPosition(Point position, bool[] visited)
+    {
+      int index = GetIndex(position);
+      return position.Y >= 0 && 
+             position.Y < maxY && 
+             position.X >= 0 && 
+             position.X < maxX && 
+             mapaSala[position.Y, position.X] != '#' && 
+             !visited[index];
+    }
+
+    /// <summary>
+    /// Extrai o caminho de pontos
+    /// </summary>
+    /// <param name="parent"></param>
+    /// <param name="source"></param>
+    /// <param name="destination"></param>
+    /// <returns></returns>
+    public List<Point> ReconstructPath(int[] parent, Point source, Point destination)
+    {
+      List<Point> path = new List<Point>();
+      int current = GetIndex(destination);
+
+      while (current != -1 && current != GetIndex(source) )
+      {
+        path.Add(GetCoordinates(current));
+        current = parent[current];
+      }
+
+      path.Add(source);
+      path.Reverse();
+
+      return path;
+    }
+  }
+  
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,91 +1,187 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
 using System.Threading;
 
 namespace ConsoleApp2
 {
   class Program
   {
+    /// <summary>
+    /// Mapa da sala com obstáculos
+    /// </summary>
+    static char[,] mapaSala = new char[,]
+    {
+            { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' },
+            { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', '#' },
+            { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', '#' },
+            { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', '#' },
+            { '#', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+            { '#', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+            { '#', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+            { '#', ' ', ' ', ' ', '#', '#', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+            { '#', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+            { '#', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+            { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+            { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#', '#', ' ', ' ', ' ', '#' },
+            { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', '#', ' ', ' ', ' ', '#' },
+            { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', '#', ' ', ' ', ' ', '#' },
+            { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', '#', ' ', ' ', ' ', '#' },
+            { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#', '#', ' ', ' ', ' ', '#' },
+            { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#', '#' },
+            { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ' },
+            { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ' },
+            { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ' },
+            { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', ' ' }
+    };
+
+    /// <summary>
+    /// Log de execução
+    /// </summary>
+    static StringBuilder sbOutput;
+
+    /// <summary>
+    /// Pilha de pontos para memorizar as posições pendentes
+    /// </summary>
+    static Stack<Point> pilhaPontosPendentes;
+
     static void Main(string[] args)
     {
-      // Definir a sala e a posição inicial do robô
-      char[,] sala = new char[,]
-      {
-            { 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' },
-            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', ' ', ' ', 'X' },
-            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', ' ', ' ', 'X' },
-            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', ' ', ' ', 'X' },
-            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', ' ', ' ', 'X' },
-            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', ' ', ' ', 'X' },
-            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', ' ', ' ', 'X' },
-            { 'X', ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
-            { 'X', ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
-            { 'X', ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
-            { 'X', ' ', ' ', ' ', 'X', 'X', 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
-            { 'X', ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
-            { 'X', ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
-            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
-            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', 'X', 'X', ' ', ' ', ' ', 'X' },
-            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', 'X', ' ', ' ', ' ', 'X' },
-            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', 'X', ' ', ' ', ' ', 'X' },
-            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', 'X', ' ', ' ', ' ', 'X' },
-            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', 'X', 'X', ' ', ' ', ' ', 'X' },
-            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
-            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
-            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
-            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
-            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
-            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
-            { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
-            { 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' }
-      };
-      int startX = 1;
-      int startY = 1;
+      Console.ReadKey();
 
-      // Executar o algoritmo de busca em profundidade
-      BuscaProfundidade(sala, startX, startY);
+      sbOutput = new StringBuilder();
 
-      // Imprimir a sala após a busca
-      ImprimirSala(sala);
+      pilhaPontosPendentes = new Stack<Point>();
+
+      var posicaoInicial = new Point(1, 1);
+
+      sbOutput.AppendLine("Executando limpeza da sala...");
+
+      var posicaoFinal = ExecutarLimpeza(posicaoInicial);
+
+      sbOutput.AppendLine("Limpeza finalizada!");
+
+      ImprimirSala(mapaSala);
+
+      Thread.Sleep(3000);
+
+      sbOutput.AppendLine("Voltando para a base de carregamento...");
+
+      MoverAtePonto(posicaoFinal, posicaoInicial);
+
+      sbOutput.AppendLine("Carregando bateria...");
+
+      ImprimirSala(mapaSala);
+
+      Console.ReadKey();
     }
 
-    static void BuscaProfundidade(char[,] sala, int x, int y)
+    /// <summary>
+    /// Executa o movimento de limpeza do robõ
+    /// </summary>
+    /// <param name="startX"></param>
+    /// <param name="startY"></param>
+    static Point ExecutarLimpeza(Point posicaoInicial)
     {
-      int altura = sala.GetLength(0);
-      int largura = sala.GetLength(1);
+      var posicaoAtual = posicaoInicial;
+      Point posicaoAnterior;
 
+      
+      EmpilhaPosicaoPendente(posicaoAtual, mapaSala);
+
+      while (pilhaPontosPendentes.Count > 0)
+      {
+        var posicaoProxima = pilhaPontosPendentes.Pop();
+
+        // Verificar se a posição atual é válida
+        if (mapaSala[posicaoProxima.Y, posicaoProxima.X] != ' ')
+        {
+          continue;
+        }
+
+        posicaoAnterior = posicaoAtual;
+        posicaoAtual = posicaoProxima;
+
+        // A próxima posição não é vizinha ?
+        if (Math.Abs(posicaoAnterior.X - posicaoAtual.X) > 1 ||
+            Math.Abs(posicaoAnterior.Y - posicaoAtual.Y) > 1)
+        {
+          // Deve pegar o menor caminho (Dijkstra) para evitar o salto de posição
+          MoverAtePonto(posicaoAnterior, posicaoAtual);
+        }
+
+        // Imprimir robô aspirador
+        mapaSala[posicaoAtual.Y, posicaoAtual.X] = 'O';
+
+        ImprimirSala(mapaSala);
+
+        // Marcar a posição atual como visitada
+        mapaSala[posicaoAtual.Y, posicaoAtual.X] = '*';
+
+        EmpilhaPosicaoPendente(new Point(posicaoAtual.X, posicaoAtual.Y + 1), mapaSala); // Baixo
+        EmpilhaPosicaoPendente(new Point(posicaoAtual.X, posicaoAtual.Y - 1), mapaSala); // Cima
+        EmpilhaPosicaoPendente(new Point(posicaoAtual.X + 1, posicaoAtual.Y), mapaSala); // Direita
+        EmpilhaPosicaoPendente(new Point(posicaoAtual.X - 1, posicaoAtual.Y), mapaSala); // Esquerda
+      }
+
+      mapaSala[posicaoAtual.Y, posicaoAtual.X] = 'O';
+      ImprimirSala(mapaSala);
+
+      return posicaoAtual;
+    }
+
+    static void MoverAtePonto(Point posicaoInicial, Point posicaoFinal)
+    {
+      var dijkstra = new Dijkstra(mapaSala);
+      List<Point> path = dijkstra.GetPath(posicaoInicial, posicaoFinal);
+
+      // Movimenta até a posição pendente
+      foreach (var position in path)
+      {
+        // Imprimir robô aspirador
+        mapaSala[position.Y, position.X] = 'O';
+
+        ImprimirSala(mapaSala);
+
+        if (position == posicaoFinal)
+        {
+          break;
+        }
+
+        // Marcar a posição atual como visitada
+        mapaSala[position.Y, position.X] = '*';
+      }
+    }
+
+    static void EmpilhaPosicaoPendente(Point posicao, char[,] mapaSala)
+    {
       // Verificar se a posição atual é válida
-      if (x < 0 || x >= largura || y < 0 || y >= altura || sala[y, x] != ' ')
+      if (mapaSala[posicao.Y, posicao.X] != ' ')
       {
         return;
       }
-
-      // Marcar a posição atual como visitada
-      sala[y, x] = '*';
-      ImprimirSala(sala);
-
-      // Chamar recursivamente a busca em profundidade para as posições adjacentes
-      BuscaProfundidade(sala, x - 1, y); // Esquerda
-      BuscaProfundidade(sala, x + 1, y); // Direita
-      BuscaProfundidade(sala, x, y - 1); // Cima
-      BuscaProfundidade(sala, x, y + 1); // Baixo
+      pilhaPontosPendentes.Push(posicao);
     }
 
-    static void ImprimirSala(char[,] sala)
+    static void ImprimirSala(char[,] mapaSala)
     {
       Console.Clear();
 
-      int altura = sala.GetLength(0);
-      int largura = sala.GetLength(1);
+      int altura = mapaSala.GetLength(0);
+      int largura = mapaSala.GetLength(1);
 
       for (int y = 0; y < altura; y++)
       {
         for (int x = 0; x < largura; x++)
         {
-          Console.Write(sala[y, x]);
+          Console.Write(mapaSala[y, x]);
         }
         Console.WriteLine();
       }
+
+      Console.WriteLine(sbOutput.ToString());
+
       Thread.Sleep(100);
     }
   }
